0c75efde8639b1949a96859fda3b1eb8151d1877
Shiny <106364725+ShinyBread@users.noreply.github.com>
2023-09-27 22:23:02 -0300

    google auth added

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,16 @@
+<script lang="ts">
+    import { auth } from "$lib/firebase";
+    import { GoogleAuthProvider, signInWithPopup,signOut } from "firebase/auth";
+    
+    async function signInWithGoogle(){
+        const provider = new GoogleAuthProvider();
+        const user = await signInWithPopup(auth, provider);
+        console.log(user)
+    }
 
+</script>
 <main>
     <h1>Registro</h1>
+    <button class="btn btn-info mt-4" on:click={signInWithGoogle}>Inicia sesion con Google</button>
 </main>
 

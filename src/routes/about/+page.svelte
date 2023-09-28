2dafaed95ad0a7e6649f588dd1ef01e2a8201d55
Shiny <106364725+ShinyBread@users.noreply.github.com>
2023-09-27 22:22:45 -0300

    about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,4 @@
-<script>
-</script>
-
-<main>
-    <h1>About</h1>
+<main class="flex flex-col justify-center items-center text-center">
+    <h1 class="text-5xl mt-10">About</h1>
+    <p class="text-2xl p-20">Hola</p>
 </main>

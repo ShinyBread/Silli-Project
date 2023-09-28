26b5bd0963a3b3ad25efb6576f20e7567fe7badb
Shiny <106364725+ShinyBread@users.noreply.github.com>
2023-09-28 02:56:57 -0300

    Auth added

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { user } from "$lib/firebase";
+</script>
+<div class="flex flex-col items-center justify-center text-center">  
+    {#if $user}
+    <slot />
+    {:else}
+        <p class="text-warning text-3xl mb-4">
+            Para poder ingresar un nick, debes logearte
+        </p>
+        <a class="btn btn-info mt-5 text-black" href="/login">Volver</a>
+    {/if}
+</div>
--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,7 +1,29 @@
-<script>
+<script lang="ts">
+    import AuthCheck from "$lib/components/AuthCheck.svelte"
+    import {db, user } from "$lib/firebase"
+    import { doc, getDoc, writeBatch } from "firebase/firestore";
     
+    let username = ""
+    let loading = false;
+    let isAvailable = false;
+    
+    async function checkAvailability(){}
+    
+    async function confirmUsername(){}
 </script>
-
-<main>
-    <h1>Username</h1>
-</main>
+<AuthCheck>
+    <main>
+        <h1 class="text-xl p-5">Escribe tu nick</h1>
+        <form on:submit|preventDefault={confirmUsername}>
+            <input 
+            type="text" 
+            placeholder="Tuki ejemplo..." 
+            class="input input-success hover:input-bordered w-full max-w-xs mt-10"
+            bind:value={username}
+            on:input={checkAvailability}
+            />
+            <p>Disponible? {isAvailable}</p>
+            <button class="btn btn-success text-sm text-black">Estas segur@ que quieres {username} como nick?</button>
+        </form>
+    </main>
+</AuthCheck>

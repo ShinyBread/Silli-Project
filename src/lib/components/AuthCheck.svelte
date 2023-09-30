972f7966d4e33106d6b69584051528cb3a571367
Shiny <106364725+ShinyBread@users.noreply.github.com>
2023-09-30 00:50:05 -0300

    svelte-legos added

--- src/lib/components/AuthCheck.svelte
+++ src/lib/components/AuthCheck.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
     import { user } from "$lib/firebase";
+    import { alertAction } from "svelte-legos";
+
+function onClose() {
+  // handle on alert close
+}
+
+function onOk() {
+  // handle on ok click
+}
+
 </script>
 <div class="flex flex-col items-center justify-center text-center">  
     {#if $user}
     <slot />
     {:else}
         <p class="text-warning text-3xl mb-4">
-            Para poder ingresar un nick, debes logearte
+            Ups!
         </p>
-        <a class="btn btn-info mt-5 text-black" href="/login">Volver</a>
+        <a class="btn btn-info mt-5 text-black" href="/login"use:alertAction={{
+            title: "No te has logeado",
+            description: "Para poder ingresar, debes logearte!",
+            onClose,
+            onOk
+          }}>Volver</a>
     {/if}
 </div>

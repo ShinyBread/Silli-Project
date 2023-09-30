8427de61adaa5a1a4d72c67a1c9dce5b45560c20
Shiny <106364725+ShinyBread@users.noreply.github.com>
2023-09-30 00:51:57 -0300

    UsernameForm creation and implementation

---
+++ src/lib/components/UsernameForm.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+    import {db, user } from "$lib/firebase"
+    import { doc, getDoc, writeBatch } from "firebase/firestore";
+    
+    let username = "";
+    let loading = false;
+    let isAvailable = false;
+    let debounceTimer: NodeJS.Timeout;
+
+    const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+    $: isValid = username?.length > 2 && username.length < 16 && re.test(username);
+    $: isTouched = username.length > 0;
+    $: isTaken = isValid && !isAvailable && !loading
+
+    async function checkAvailability(){
+        isAvailable = false;
+        clearTimeout(debounceTimer);
+        loading = true;
+
+        debounceTimer = setTimeout(async()=>{
+            console.log("revisando disponibilidad de:", username);
+            const ref = doc(db, "usernames", username);
+            const exists = await getDoc(ref).then((doc) =>doc.exists());
+
+            isAvailable = !exists;
+            loading = false;
+        
+        }, 1500);
+        
+    }
+    
+    async function confirmUsername(){
+        console.log("revisando disponibilidad de:", username);
+        const batch = writeBatch(db);
+        batch.set(doc(db, "usernames", username), {uid : $user?.uid});
+        batch.set(doc(db,"users" , $user!.uid), {
+            username,
+            photoURL : $user?.photoURL ?? null,
+            published : true,
+            bio: ' Tuji ejemplo bio',
+            links:[
+                {
+                    title: ' Link Ejemplo',
+                    url: 'https://miau.com',
+                    icon: 'custom'
+                }
+            ]
+        });
+        await batch.commit();
+    }
+</script>
+
+
+
+
+
+<form on:submit|preventDefault={confirmUsername}>
+    <input 
+    type="text" 
+    placeholder="Tuki ejemplo..." 
+    class="input input-success hover:input-bordered w-full max-w-xs mt-10"
+    bind:value={username}
+    on:input={checkAvailability} 
+    class:input-error={(!isValid && isTouched)}
+    class:input-warning={isTaken}
+    class:input-success={isAvailable && isValid && !loading}
+    />
+    
+    <div class="my-4 min-h-16 px-8 w-full">
+        {#if loading}
+          <div class="flex alert alert-info mt-4 text-black text-center justify-center text-sm">
+            <p>Verificando nick {username}</p>
+            <span class="loading loading-spinner text-black"></span>
+          </div>
+        {/if}
+    
+        {#if !isValid && isTouched}
+          <p class="text-warning text-sm">
+            Asegurate que tenga al menos 3 caracteres y sea alfanumerico!
+          </p>
+        {/if}
+    
+        {#if isValid && !isAvailable && !loading}
+          <p class="text-green text-sm">
+            {username} no esta disponible :c
+          </p>
+        {/if}
+    
+        {#if isAvailable && isValid}
+        <div class="flex alert alert-success mt-4 text-black text-center justify-center text-sm">
+            <p>" {username} " disponible!</p>
+            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-5 w-5" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        </div>
+        <button class="btn btn-warning text-lm text-black mt-4">Confirmar</button>
+        {/if}
+      </div>
+</form>
--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,29 +1,11 @@
 <script lang="ts">
     import AuthCheck from "$lib/components/AuthCheck.svelte"
-    import {db, user } from "$lib/firebase"
-    import { doc, getDoc, writeBatch } from "firebase/firestore";
-    
-    let username = ""
-    let loading = false;
-    let isAvailable = false;
-    
-    async function checkAvailability(){}
-    
-    async function confirmUsername(){}
+    import UsernameForm from "$lib/components/UsernameForm.svelte";
+
 </script>
 <AuthCheck>
     <main>
         <h1 class="text-xl p-5">Escribe tu nick</h1>
-        <form on:submit|preventDefault={confirmUsername}>
-            <input 
-            type="text" 
-            placeholder="Tuki ejemplo..." 
-            class="input input-success hover:input-bordered w-full max-w-xs mt-10"
-            bind:value={username}
-            on:input={checkAvailability}
-            />
-            <p>Disponible? {isAvailable}</p>
-            <button class="btn btn-success text-sm text-black">Estas segur@ que quieres {username} como nick?</button>
-        </form>
+        <UsernameForm />
     </main>
 </AuthCheck>

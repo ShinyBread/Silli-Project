9e7a08ed789e891388c4759f5b0de243e839de75
Shiny <106364725+ShinyBread@users.noreply.github.com>
2023-10-04 19:56:15 -0300

    9xl

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 <div class="hero min-h-screen">
   <div class="hero-content text-center">
     <div class="max-w-md">
-      <h1 class="text-6xl font-bold text-base-100">Silli</h1>
+      <h1 class="text-9xl font-bold text-base-100">Silli</h1>
       <p class="py-6 text-base-100">Estaba la pájara pinta,
           Sentada en su verde limón,
           Con el pico cortaba la rama,

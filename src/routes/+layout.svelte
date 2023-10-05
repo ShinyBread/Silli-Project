2a9b19e7442025f28c6e5ac1ec65c82b1830f080
Shiny <106364725+ShinyBread@users.noreply.github.com>
2023-10-04 23:42:19 -0300

    ViewTransition added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,20 @@
     import "../app.css";
     import NavBar from "$lib/components/NavBar.svelte";
     import Footer from "$lib/components/Footer.svelte";
+    import ViewTransition from "$lib/components/ViewTransition.svelte"
 </script>
 
-<NavBar></NavBar>
-<body class="min-h-screen bg-gradient-to-b from-pink-200 via-pink-300 to-pink-400">
-    <slot />
-</body>  
-<Footer></Footer>
+
+<svelte:head>
+    <title>Silli</title>
+</svelte:head>
+
+<div class=app>
+    <ViewTransition />
+    <NavBar />
+    <main class="min-h-screen bg-gradient-to-b from-pink-200 via-pink-300 to-pink-400">
+        <slot />
+    </main>
+    <Footer />
+</div>
+

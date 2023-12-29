f3fc008de3b4bb31bd300e753d3d1e399217fb55
ShinyBread <shinybreadjp@gmail.com>
2023-12-28 22:19:48 -0300

    details

---
+++ src/lib/components/UserLink.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let icon = 'default';
+    export let url = 'foo';
+    export let title = 'some cool title';
+</script>
+
+
+<a href={url} class="stack w-full max-w-md text-center bg-primary flex justify-center items-center p-4 rounded-lg not-prose no-underline shadow-white shadow-md">
+  <img src={`/${icon}.png`} alt={icon} width="32" height="32" class="w-8" />
+  <span class="text-2xl text-white font-bold">{title}</span>
+</a>
---
+++ src/routes/[username]/edit/+page.svelte
@@ -0,0 +1,147 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import UserLink from "$lib/components/UserLink.svelte";
+    import { db, userData, user } from "$lib/firebase";
+    import {arrayRemove,arrayUnion,doc,setDoc,updateDoc,} from "firebase/firestore";
+    import { writable } from "svelte/store";
+    import SortableList from "$lib/components/SortableList.svelte";
+  
+    const icons = [
+      "Twitter",
+      "YouTube",
+      "TikTok",
+      "LinkedIn",
+      "GitHub",
+      "Custom",
+    ];
+
+    const formDefaults = {
+      icon: "custom",
+      title: "",
+      url: "https://",
+    };
+
+    const formData = writable(formDefaults);
+  
+    let showForm = false;
+  
+    $: urlIsValid = $formData.url.match(/^(ftp|http|https):\/\/[^ "]+$/);
+    $: titleIsValid = $formData.title.length < 20 && $formData.title.length > 0;
+    $: formIsValid = urlIsValid && titleIsValid;
+
+  
+    async function addLink(e: SubmitEvent) {
+      const userRef = doc(db, "users", $user!.uid);
+  
+      await updateDoc(userRef, {
+        links: arrayUnion({
+          ...$formData,
+          id: Date.now().toString(),
+        }),
+      });
+  
+      formData.set({
+        icon: "",
+        title: "",
+        url: "",
+      });
+  
+      showForm = false;
+    }
+
+  
+    async function deleteLink(item: any) {
+      const userRef = doc(db, "users", $user!.uid);
+      await updateDoc(userRef, {
+        links: arrayRemove(item),
+      });
+    }
+  
+    function cancelLink() {
+      formData.set(formDefaults);
+      showForm = false;
+    }
+    function sortList(e: CustomEvent) {
+      const newList = e.detail;
+      const userRef = doc(db, "users", $user!.uid);
+      setDoc(userRef, { links: newList }, { merge: true });
+    }
+  
+</script>
+  
+  <main class="max-w-xl mx-auto">
+    {#if $userData?.username == $page.params.username}
+      <h1 class="mx-2 text-2xl font-bold mt-8 mb-4 text-center text-black">
+        Edita tu perfil!
+      </h1>
+
+      <SortableList list={$userData?.links} on:sort={sortList} let:item let:index>
+        <div class="group relative">
+          <UserLink {...item} />
+          <button
+            on:click={() => deleteLink(item)}
+            class="btn btn-xs btn-error invisible group-hover:visible transition-all absolute -right-6 bottom-10 text-white "
+            >Eliminar</button
+          >
+        </div>
+      </SortableList>
+
+      {#if showForm}
+        <form
+          on:submit|preventDefault={addLink}
+          class="bg-base-200 p-6 mt-20 w-full mx-auto rounded-xl shadow-error shadow-2xl text-white"
+        >
+          <select
+            name="icon"
+            class="select select-sm"
+            bind:value={$formData.icon}
+          >
+
+            {#each icons as icon}
+              <option class="text-black" value={icon.toLowerCase()}>{icon}</option>
+            {/each}
+          </select>
+          <input
+            name="title"
+            type="text"
+            placeholder="Title"
+            class="input input-sm"
+            bind:value={$formData.title}
+          />
+          <input
+            name="url"
+            type="text"
+            placeholder="URL"
+            class="input input-sm"
+            bind:value={$formData.url}
+          />
+          <div class="my-4">
+            {#if !titleIsValid}
+              <p class="text-error text-xs">Titulo invalido</p>
+            {/if}
+            {#if !urlIsValid}
+              <p class="text-error text-xs">URL invalida</p>
+            {/if}
+            {#if formIsValid}
+              <p class="text-success text-xs">Nice!</p>
+            {/if}
+          </div>
+  
+          <button
+            disabled={!formIsValid}
+            type="submit"
+            class="btn btn-success block">Agregar!</button
+          >
+  
+          <button type="button" class="btn btn-xs my-4" on:click={cancelLink}>Cancel</button>
+        </form>
+      {:else}
+        <button
+          on:click={() => (showForm = true)}
+          class="btn btn-outline btn-info block mx-auto my-4"
+        >
+          Agrega Link
+        </button>
+      {/if}
+    {/if}
+  </main>
